72b7f06af9f188d492400db42c6bca1d854a94e9
Luv Aggarwal <luvkaggarwal@gmail.com>
2017-08-26 00:13:21 +0530

    LC for FirstPass Detected

--- assembler.py
+++ assembler.py
@@ -1,4 +1,30 @@
+"""
+Code to implement a 2 Pass Assembler in Python that will take in Assembly Code and Produce a Binary Code
+"""
+
+def processLine(line):
+    return ""
+        
+
+# Function to generate symbol table using the first pass
+def firstPass(code):
+    symbolTable = []
+    counter = 0
+    flag = -1
+    for line in code:
+        for word in line.split():
+            if word == 'ORG':
+                flag = 1
+            elif flag == 1:
+                counter = int(word) - 1
+                flag = -1
+            elif word == 'DEC' or word == 'HEX':
+                symbolTable.append([processLine(line),counter])
+        counter += 1            
+    return symbolTable
+
+# Main working of the code begins here
 code = open("AssemblyLanguageCode.txt")
 
-for word in code.read().split():
-    print word
+symbolTable = firstPass(code)
+print symbolTable
