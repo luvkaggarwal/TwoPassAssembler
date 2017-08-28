7aa5b99d2157c042eecae0ec974fcaadbba55e24
Luv Aggarwal <luvkaggarwal@gmail.com>
2017-08-28 16:40:02 +0530

    Completed First Pass

--- assembler.py
+++ assembler.py
@@ -3,28 +3,32 @@ Code to implement a 2 Pass Assembler in Python that will take in Assembly Code a
 """
 
 import pandas as pd
-
-# Function to return label
-def processLine(line):
-    return line.split(',')[0]
     
 # Function to generate symbol table using the first pass
-def firstPass(code):
+def firstPass(code,instructionSet):
     symbolTable = []
     counter = 0
-    flag = -1
-
+    
     for line in code:
         if line.split()[0] == 'ORG':
-            counter = int( line.split()[1] ) - 1
-        elif len( line.split(',') ) > 1 :
-            symbolTable.append([line.split(',')[0],counter])
-        counter += 1            
+            counter = int( line.split()[1] )
+        elif len( line.split(':') ) > 1 :
+            symbolTable.append([line.split(':')[0],counter%100,counter/100])
+            counter += 1
+        elif len( instructionSet[ instructionSet['Mnemonic'] == line ] ) > 1:
+            counter +=1
+        elif len( line.split(',') ) > 1:
+            word = line.split(',')[1]
+            if len( word ) == 3:
+                counter += 2
+            else:
+                counter +=3
+        else:
+            counter += 3
     return symbolTable
 
 # Main working of the code begins here
 code = open("SampleCode1.txt")
 instructionSet = pd.read_csv("8085InstructionSet.csv")
-symbolTable = firstPass(code)
-print code
-print instructionSet
+symbolTable = firstPass(code,instructionSet)
+print symbolTable
