dac3b4bf8a247d749a55e595d6db811133f7d414
Luv Aggarwal <luvkaggarwal@gmail.com>
2017-08-29 12:31:02 +0530

    Documentation Added

--- assembler.py
+++ assembler.py
@@ -3,7 +3,15 @@ Code to implement a 2 Pass Assembler in Python that will take in Assembly Code a
 """
 
 import pandas as pd
-    
+
+# Functoin to add binarycode and counter value for a given hexcode
+def symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions):
+    bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
+    hexcodes.append(hexcode)
+    bincodes.append(bincode)
+    counterPositions.append(hex(counter)[2:])
+    return hexcodes,bincodes,counterPositions
+
 # Function to generate symbol table using the first pass
 def firstPass(instructionSet):
     symbols = []
@@ -12,25 +20,42 @@ def firstPass(instructionSet):
     counter = 0
     code = open("SampleCode1.txt")
     for line in code:
+
+        # Remove the carriage return from each line
         line = line.strip()
+
+        # If referenced location, store the reference with location counter value
+        # Eg - LOOP: DCX H  // Here, store 'LOOP' with LC
         if len( line.split(':') ) > 1 :
             hexcode = hex(counter)[2:]
             symbols.append(line.split(':')[0])
             lowerByte.append(hexcode[2:])
             higherByte.append(hexcode[:2])
             line = line.split(':')[1]
+
+            # Remove reference to get actual line of code
+            # Eg - LOOP: DCX H  // Here, remove 'LOOP: ' to get the actual line of code
             line = line[2:]
+
+        # No Further commands available
         if line == 'END':
             break
         elif line.split()[0] == 'ORG':
+            # Set Location Counter
             counter = int( line.split()[1],16 )
         elif len( instructionSet[ instructionSet['Mnemonic'] == line ] ) > 0:
-            counter +=1
+            # For direct instructions
+            # Eg - DCX H
+            counter += 1
         elif len( line.split(',') ) > 1:
+            # For instructions with reference to Memory or direct values
+            # Eg - LXI H,XXXXH ; MVI A,XXH
             word = line.split(',')[1]
             if len( word ) == 3:
+                # Direct Value Reference
                 counter += 2
             else:
+                # Memory Reference
                 counter +=3
         else:
             counter += 3
@@ -49,78 +74,85 @@ def secondPass(instructionSet,symbolFrame):
     lineOfCode = []
     code = open("SampleCode1.txt")
     for line in code:
+
+        # Remove the carriage return from each line
         line = line.strip()
+
+        # If referenced location, remove the reference
+        # Eg - LOOP: DCX H  // Here, remove 'LOOP: ' to get the actual line of code
         if len( line.split(':') ) > 1 :
             line = line.split(':')[1]
             line = line[1:]
+
+        # No Further commands available
         if line == 'END':
             break
         elif line.split()[0] == 'ORG':
+            # Set Location Counter
             counter = int( line.split()[1],16 )
         elif len( instructionSet[ instructionSet['Mnemonic'] == line ] ) > 0:
+            # For direct instructions
+            # Eg - DCX H
             hexcode = instructionSet[ instructionSet['Mnemonic'] == line ]['Hex'].to_string(index=False)
-            bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-            hexcodes.append(hexcode)
-            bincodes.append(bincode)
-            counterPositions.append(hex(counter)[2:])
+            hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
             lineOfCode.append(line)
             counter += 1
         elif len( line.split(',') ) > 1:
+            # For instructions with reference to Memory or direct values
+            # Eg - LXI H,XXXXH ; MVI A,XXH
+
+            # Find HexCode for Mnemonic
             hexcode = instructionSet[ instructionSet['Mnemonic'] == line.split(',')[0] ]['Hex'].to_string(index=False)
-            bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-            hexcodes.append(hexcode)
-            bincodes.append(bincode)
-            counterPositions.append(hex(counter)[2:])
+            hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
             lineOfCode.append(line.split(',')[0])
             counter += 1
+
+            # Find HexCode for Memory Reference/Direct Value Reference
             word = line.split(',')[1]
             if len( word ) == 3:
+                # Direct Value Reference
                 hexcode = word[:2]
-                bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-                hexcodes.append(hexcode)
-                bincodes.append(bincode)
-                counterPositions.append(hex(counter)[2:])
+                hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
                 lineOfCode.append(line.split(',')[0])
                 counter += 1
             else:
+                # Memory Reference
+
+                # For Lower Byte
                 hexcode = word[2:4]
-                bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-                hexcodes.append(hexcode)
-                bincodes.append(bincode)
-                counterPositions.append(hex(counter)[2:])
+                hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
                 lineOfCode.append(hexcode)
                 counter += 1
+
+                # For Higher Byte
                 hexcode = word[:2]
-                bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-                hexcodes.append(hexcode)
-                bincodes.append(bincode)
-                counterPositions.append(hex(counter)[2:])
+                hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
                 lineOfCode.append(hexcode)
                 counter += 1
         else:
+            # For statements like JNZ LOOP
+
+            # Find HexCode for Mnemonic
             hexcode = instructionSet[ instructionSet['Mnemonic'] == line.split()[0] ]['Hex'].to_string(index=False)
-            bincode = ( bin(int(hexcode, 16))[2:] ).zfill(8)
-            hexcodes.append(hexcode)
-            bincodes.append(bincode)
-            counterPositions.append(hex(counter)[2:])
+            hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
             lineOfCode.append(line.split()[0])
             counter += 1
+
+            # Find HexCodes for Symbol(Reference to another location) from SymbolFrame
+            # For Lower Byte
             symbol = symbolFrame[ symbolFrame['Symbol'] == line.split()[1] ]
-            byte = symbol['lowerByte'].to_string(index=False)
-            bincode = ( bin(int(byte, 16))[2:] ).zfill(8)
-            hexcodes.append(byte)
-            bincodes.append(bincode)
-            counterPositions.append(hex(counter)[2:])
+            hexcode = symbol['lowerByte'].to_string(index=False)
+            hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
             lineOfCode.append(symbol['Symbol'].to_string(index=False))
             counter += 1
-            byte = symbol['higherByte'].to_string(index=False)
-            bincode = ( bin(int(byte, 16))[2:] ).zfill(8)
-            hexcodes.append(byte)
-            bincodes.append(bincode)
-            counterPositions.append(hex(counter)[2:])
+            
+            # For Higher Byte
+            hexcode = symbol['higherByte'].to_string(index=False)
+            hexcodes,bincodes,counterPositions = symbolParser(counter,hexcode,hexcodes,bincodes,counterPositions)
             lineOfCode.append(symbol['Symbol'].to_string(index=False))
             counter += 1
-    
+
+    # Compile into a single Data Frame
     frame = pd.DataFrame()
     frame['Counter'] = counterPositions
     frame['Code'] = lineOfCode
